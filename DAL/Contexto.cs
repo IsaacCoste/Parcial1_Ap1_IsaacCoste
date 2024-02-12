1bd7e64c45f6c8a278e850ad1942ab2edac737ee
Isaac Patricio Coste Burgos <isaac_coste@ucne.edu.do>
2024-02-12 18:41:15 -0400

    Cree el contexto

---
+++ DAL/Contexto.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Parcial1_Ap1_IsaacCoste.Models;
+
+namespace Parcial1_Ap1_IsaacCoste.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Metas> Metas { get; set; }
+        public Contexto(DbContextOptions<Contexto> options) : base(options)
+        {
+        }
+    }
+}
