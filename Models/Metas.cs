1e3ad736adf3181f117b52f982a84169ea334c53
Isaac Patricio Coste Burgos <isaac_coste@ucne.edu.do>
2024-02-12 18:24:46 -0400

    Cree el Modelo de las Metas

---
+++ Models/Metas.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Parcial1_Ap1_IsaacCoste.Models
+{
+    public class Metas
+    {
+        [Key]
+        public int MetaId { get; set; }
+        [Required(ErrorMessage = "Es obligatorio introducir la fecha.")]
+        public DateTime Fecha { get; set; }
+        [Required(ErrorMessage = "Es obligatorio introducir la Descripción")]
+        public string? Descripción { get; set; }
+        [Required(ErrorMessage = "Es obligatorio introducir el Monto")]
+        public decimal Monto { get; set; }
+    }
+}
